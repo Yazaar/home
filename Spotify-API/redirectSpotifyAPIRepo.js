4b740ee59a449bd2a87dd5672648ff8ee503b970
Yazaar <41300736+Yazaar@users.noreply.github.com>
2021-09-28 12:33:55 +0200

    Spotify-API redirect

---
+++ Spotify-API/redirectSpotifyAPIRepo.js
@@ -0,0 +1,8 @@
+(function () {
+    var current = window.location.href;
+    var from = '/spotify-api'
+    var to = '/Spotify-Overlay'
+    var fIndex = current.toLowerCase().indexOf(from);
+    var target = current.substr(0, fIndex) + to + current.substr(fIndex + from.length);
+    window.location = target;
+})();
